7d2db6e4933d0ad962eb379c8a82e3b23b500dfb
Terra <106891646+TerraInCaelo@users.noreply.github.com>
2024-08-30 17:57:39 +0200

    Update PlayerStats.cs

--- Assets/TerraFiles/PlayerStats.cs
+++ Assets/TerraFiles/PlayerStats.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour{
+    public static PlayerStats instance;
     public float speed = 1;
     public float jumpForce = 2;
+    private void Awake(){
+        instance = this;
+    }
 }
