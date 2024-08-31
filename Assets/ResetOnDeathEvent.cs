166b7cf48935c4ab4da77ca11499620bc0e0697a
aziomo <przytulaalbert@gmail.com>
2024-08-31 22:21:06 +0200

    out of bounds death added

---
+++ Assets/ResetOnDeathEvent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetOnDeathEvent : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject player;
+
+
+    void ResetLevel(){
+
+    }
+
+
+    
+}
