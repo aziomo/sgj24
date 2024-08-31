6db5a4c0968e47843749a8b9fd0891d684020b53
Terra <106891646+TerraInCaelo@users.noreply.github.com>
2024-08-31 22:37:18 +0200

    Update GameManager.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,20 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour{
     public static GameManager Instance;
-    public int level = 0;
+    public int level = 1;
     void Awake(){
         Instance = this;
         DontDestroyOnLoad(this);
     }
     public void ResetLevel(){
-        
+        SceneManager.LoadScene(level);
         Destroy(this);
     }
-
-    void Update(){
-        
+    private void PlayerWon(){
+        level++;
+        SceneManager.LoadScene(level);
     }
 }
