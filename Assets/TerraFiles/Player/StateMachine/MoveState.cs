553e0bd3a3fef3ec9b5b5bcc9a1ccb39e262e0ed
Terra <106891646+TerraInCaelo@users.noreply.github.com>
2024-08-31 13:23:42 +0200

    w

--- Assets/TerraFiles/Player/StateMachine/MoveState.cs
+++ Assets/TerraFiles/Player/StateMachine/MoveState.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveState : State{
+    public ParticleSystem particleRun;
+    private void Start(){
+        particleRun.gameObject.SetActive(false);
+    }
     public override void StartState(){}
     public override void UpdateState(){
         var inputMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0 , Input.GetAxisRaw("Vertical")).normalized;
@@ -10,5 +14,17 @@ public class MoveState : State{
             Move();
         }
     }
-    public override void EndState(){}
+    public override void EndState(){
+        particleRun.gameObject.SetActive(false);
+    }
+    public override void Move(){
+        var inputMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0 , Input.GetAxisRaw("Vertical"));
+        if(Input.GetKey(KeyCode.LeftShift)){
+            particleRun.gameObject.SetActive(true);
+            rb.velocity =new Vector3(inputMovement.x * stats.speed * stats.sprintMultiplier, rb.velocity.y, inputMovement.z * stats.speed* stats.sprintMultiplier);
+        }else{
+            particleRun.gameObject.SetActive(false);
+            rb.velocity =new Vector3(inputMovement.x * stats.speed, rb.velocity.y, inputMovement.z * stats.speed);
+        }
+    }
 }
