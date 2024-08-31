89d362bf2df4545070a222ca48183e25b1556365
Terra <106891646+TerraInCaelo@users.noreply.github.com>
2024-08-31 23:16:14 +0200

    Update GameManager.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour{
     public static GameManager Instance;
     public int level = 1;
+    public int[] levelWinCondition;
     void Awake(){
         Instance = this;
         DontDestroyOnLoad(this);
@@ -14,8 +16,21 @@ public class GameManager : MonoBehaviour{
         SceneManager.LoadScene(level);
         Destroy(this);
     }
+    public void ConditionCalled(){
+        levelWinCondition[level - 1] -= 1;
+        if(0 == levelWinCondition[level - 1]){
+            PlayerWon();
+        }
+    }
     private void PlayerWon(){
         level++;
+        if(level - 1 == levelWinCondition.Length){
+            Won();
+            return;
+        }
         SceneManager.LoadScene(level);
+    }
+    private void Won(){
+        Debug.Log("won");
     }
 }
