64313b83a0ffd2be22968b8f9fbe49b009d91953
aziomo <przytulaalbert@gmail.com>
2024-08-31 14:01:38 +0200

    lasso

---
+++ Assets/AlbertoFiles/scena lasso/LassoDrawHandle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LassoDrawHandle : MonoBehaviour
+{
+    public GameObject player;
+    public float knotOrbitSpeed = 10.0f;
+    public GameObject handle;
+    public GameObject handleKnot;
+    public GameObject lassoHoop;
+
+    public Transform lassoHandleStartPoint;
+    public Transform lassoHandleEndPoint;
+
+    void Start()
+    {
+        handle.GetComponent<LineRenderer>().SetPosition(0, lassoHandleStartPoint.position);
+    }
+
+
+    void Update()
+    {
+        handle.GetComponent<LineRenderer>().SetPosition(0, lassoHandleStartPoint.position);
+        handle.GetComponent<LineRenderer>().SetPosition(1, lassoHandleEndPoint.position);
+    }
+}
