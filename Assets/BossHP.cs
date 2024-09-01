a1ea236192e0b043ca112837c5d4095d2bd79e1a
Straditarion <31244259+Straditarion@users.noreply.github.com>
2024-09-01 09:50:15 +0200

    Kefir and blood vfx

--- Assets/BossHP.cs
+++ Assets/BossHP.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class BossHP : MonoBehaviour, IHealth{
     public float hp = 100;
+    [SerializeField] private GameObject _splashVfx;
+    [SerializeField] private GameObject _deathVfx;
     public void TakeDamage(float damage){
         hp -= damage;
         if(hp <= 0){
             // tu enumerator do animacji śmierci kerfusia
             GameManager.Instance.ConditionCalled();
+            Instantiate(_deathVfx, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(_splashVfx, Sausage.Active.transform.position, Quaternion.identity);
         }
     }
     public void TakeHeal(float heal){}
