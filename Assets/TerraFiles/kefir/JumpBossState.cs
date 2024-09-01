65530b4014c254e341435cbe3bbdd13c868c5891
Bartosz Janiak <bartosz.janiak@gmail.com>
2024-09-01 12:28:56 +0200

    Kefir sfx

--- Assets/TerraFiles/kefir/JumpBossState.cs
+++ Assets/TerraFiles/kefir/JumpBossState.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class JumpBossState : State{
     public ParticleSystem jumpParticle;
+    public AudioClip hitGround;
     public override void StartState(){
         jumpParticle.Stop();
         StartCoroutine(IDK());
@@ -15,6 +16,7 @@ public class JumpBossState : State{
         yield return new WaitForSeconds(anim.runtimeAnimatorController.animationClips[1].length - .3f);
         jumpParticle.Play();
         UltimateCamera.instance.StartCameraShake(.6f, 50);
+        AudioSource.PlayClipAtPoint(hitGround, transform.position);
         yield return new WaitForSeconds(.3f);
         state.ChangeState(States.BossIdle);
     }
