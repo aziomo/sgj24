d692f069a84c15f820281c6f27fe9a7c2379888d
Bartosz Janiak <bartosz.janiak@gmail.com>
2024-09-01 12:15:58 +0200

    cow sfx

--- Assets/AlbertoFiles/scena lasso/LassoCaughtEvent.cs
+++ Assets/AlbertoFiles/scena lasso/LassoCaughtEvent.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class LassoCaughtEvent : MonoBehaviour
 {
+    public AudioClip moo;
     public GameObject player;
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Catchable")) {
+            AudioSource.PlayClipAtPoint(moo, transform.position);
             other.transform.parent.GetComponent<OgreCowController>().isCaught = true;
             player.GetComponent<LassoController>().caughtCow = other.transform.parent;
         }
